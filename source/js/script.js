0b6dee1b9c8b720152d0895fec4cad94a9c74f20
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-06-17 15:30:54 +0300

    Add javascript

---
+++ source/js/script.js
@@ -0,0 +1,90 @@
+'use strict';
+
+(function () {
+  let data = [];
+
+  function fetchData(onFetchSuccess, onFetchError) {
+    fetch(
+      'https://603e38c548171b0017b2ecf7.mockapi.io/homes',
+      {
+        method: 'GET'
+      }
+    ).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    }).then((json) => {
+      onFetchSuccess(json);
+    })
+  }
+
+  function onFetchSuccess(json) {
+    data = json;
+
+    const cardTemplate = document.querySelector('#card').content;
+    const cardList = document.querySelector('.cards__list');
+
+    const TYPE_SUPPORT = 'SupportAvailable';
+    const TYPE_INDEPENDENT = 'IndependentLiving';
+
+    for (let i = 0; i < data.length; i++) {
+      const photoUrl = `https://source.unsplash.com/random/377x227?house&${i}`;
+
+      let card = data[i];
+      const cardElement = cardTemplate.cloneNode(true);
+
+      cardElement.querySelector('.card__title').textContent = card.title;
+      cardElement.querySelector('.price__information--number').textContent = card.price;
+      cardElement.querySelector('.card__text').textContent = card.address;
+
+      cardElement.querySelector('.cards__image').src = photoUrl;
+
+      cardElement.querySelector('.cards__item').setAttribute('data-id', card.id);
+      cardElement.querySelector('.cards__link').setAttribute('href', `/details/${card.id}`);
+
+      if (card.type === TYPE_SUPPORT) {
+        cardElement.querySelector('.cards__type--support').classList.remove('hidden');
+      } else if (card.type === TYPE_INDEPENDENT) {
+        cardElement.querySelector('.cards__type--independent').classList.remove('hidden');
+      }
+
+      cardList.appendChild(cardElement);
+    }
+  }
+
+  function onFetchError() {
+    //
+  }
+
+  function filter(searchValue) {
+    for (let i = 0; i < data.length; i++) {
+      let card = data[i];
+
+      let cardElement = document.querySelector(`.cards__item[data-id="${card.id}"]`);
+
+      if (card.title.toLowerCase().includes(searchValue.toLowerCase())) {
+        cardElement.classList.remove('hidden');
+      } else {
+        cardElement.classList.add('hidden');
+      }
+    }
+
+  }
+
+  fetchData(onFetchSuccess, onFetchError);
+
+  let searchField = document.querySelector('.form__input');
+  searchField.addEventListener('input', () => {
+    if (searchField.value.length === 0) {
+      document.querySelectorAll('.cards__item').forEach((cardElement) => {
+        cardElement.classList.remove('hidden');
+      });
+    }
+
+    if (searchField.value.length < 3) {
+      return false;
+    }
+
+    filter(searchField.value);
+  });
+})();
